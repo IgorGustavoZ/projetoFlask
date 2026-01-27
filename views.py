71b3834993fe561e6a44097787b388169ca09839
igorgustavo <igorzerbinatto12@gmail.com>
2026-01-27 18:13:24 -0300

    Teste de impressão

--- views.py
+++ views.py
@@ -2,9 +2,19 @@ from main import app
 from flask import render_template, request, redirect
 from flask import request
 
+import win32api
+
 @app.route("/")
 def homepage():
     return render_template('index.html')
+
+@app.route("/print", methods=['POST'])
+def print():
+    try:
+        win32api.ShellExecute(0, "print", r"C:\Users\V12 Informatica\Desktop\projetoFlask\uploads\imprimir.jpg", None, ".", 0)
+        return redirect("/")
+    except:
+        return "Erro na impressão"
 
 # @app.route("/soma", methods=['POST'])
 # def soma():
