be2a6bc27f3158b6de585e5edfac12e9b1f97ba5
igorgustavo <igorzerbinatto12@gmail.com>
2026-01-28 19:49:41 -0300

    Ajustes e comentários

--- views.py
+++ views.py
@@ -7,15 +7,18 @@ import time
 import os
 import win32api
 
-UPLOAD_FOLDER = r"uploads"
+UPLOAD_FOLDER = r"uploads" #definindo a pasta onde será salvo as imagens
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 @app.route("/")
 def homepage():
     return render_template('index.html')
 
+#exclui todos os arquivos de 'uploads'
 def excludeUpload(arquivos, caminho):
     for arquivo in arquivos:
         os.remove(os.path.join(caminho, arquivo))
+
+#imprime os arquivos do diretório uploads.
 @app.route("/upload", methods=['POST'])
 def upload():
     arquivos = os.listdir(r"C:\Users\V12 Informatica\Desktop\projetoFlask\uploads")
@@ -37,10 +40,12 @@ def upload():
     except Exception as e:
         print("Ocorreu um erro de impressão: ", e)
         return "Erro na impressão !"
+
+#Salvamento automático em uploads, chamado toda vez que o input file muda-se seu valor.
 @app.route("/save", methods=['POST'])
 def save():
     try:
-        file = request.files['arq']
+        file = request.files.get('arq') # no get () o parametro usado é o atributo name do input file.
         file.filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
         return redirect("/")
@@ -49,15 +54,3 @@ def save():
         return redirect("Erro na instalação das imagens !")
 
 
-# @app.route("/soma", methods=['POST'])
-# def soma():
-#     try:
-#         num1 = request.form.get('num1')
-#         num2 = request.form.get('num2')
-#         print(num1 + num2)
-#
-#         num1 += int(num1)
-#         num2 += int(num2)
-#         return num1 + num2
-#     except:
-#         return "erro"
